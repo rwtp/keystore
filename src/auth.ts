18d1f6c675963a121d45d3fab00931a799c5fe4f
Evan Conrad <evan@roomservice.dev>
2022-05-16 17:15:34 -0700

    include info that it doesnt cost anything

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,7 @@ import { CHALLENGE_PREFIX } from "./kv";
 export function createChallenge(nonce: string) {
   // WARNING!!! If you change this information, every user will need to re-login.
   return `
-Sign this message if you trust this application to access private information, such as names, addresses, and emails.
+Sign this message if you trust this application to access private information, such as names, addresses, and emails. It costs nothing to sign this message.
     
 URL: https://keystore.rwtp.org
 Nonce: ${nonce}`.trim();
